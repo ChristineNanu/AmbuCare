f244d9a7fb8cce6e741044a1de4fdcf8d00b7f71
christinemundi <mundichristine@gmail.com>
2024-10-23 12:01:24 +0300

    Home.js added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Welcome to AmbuCare</h1>
+            <h1>Welcome to the Ambu Care!</h1>
         </div>
     );
 };
